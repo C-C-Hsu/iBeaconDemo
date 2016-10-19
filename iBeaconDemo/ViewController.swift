082359f83acfb68f56893d86532e646c94a2cd06
Allen <Allen>
2016-10-19 21:18:11 +0800

    add locationManager func

--- iBeaconDemo/ViewController.swift
+++ iBeaconDemo/ViewController.swift
@@ -26,6 +26,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        // 設定 view 的背景顏色
+        self.view.backgroundColor = self.colors[0]
+        
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedAlways) {
+            
+            locationManager.requestAlwaysAuthorization()
+        }
+        locationManager.startRangingBeacons(in: region)
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +43,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        
+        let knownBeacons = beacons.filter {
+            $0.proximity != CLProximity.unknown
+        }
+        
+        if (knownBeacons.count > 0) {
+            
+            if (!iBeaconStatus) {
+            
+                let closestBeacon = knownBeacons[0] as CLBeacon
+                self.view.backgroundColor = self.colors[closestBeacon.minor.intValue]
+                iBeaconStatus = true
+            }
+        } else {
+            
+            if (iBeaconStatus) {
+                
+                self.view.backgroundColor = self.colors[0]
+                iBeaconStatus = false
+            }
+        }
+    }
 }
 
