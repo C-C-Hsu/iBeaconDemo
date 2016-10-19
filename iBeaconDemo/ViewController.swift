c385e06e414e2242a82b1043831769d5035ad30c
Allen <Allen>
2016-10-19 21:10:29 +0800

    add locationManager let

--- iBeaconDemo/ViewController.swift
+++ iBeaconDemo/ViewController.swift
@@ -12,6 +12,8 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
